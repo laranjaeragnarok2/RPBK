69bec4ae08160eba91d487f4e037fdee4c2e0a81
Arthur Horyu <1horyuarthur@gmail.com>
2025-11-12 17:29:31 +0000

    Não funcionou, coloque pelo link https://images.pexels.com/photos/28355

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
